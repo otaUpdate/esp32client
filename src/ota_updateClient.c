4bd185a535bb0869972eb1733ceddfee1253a5eb
Christopher Armenio <christopher.armenio@ovrengineered.com>
2018-01-13 11:42:03 -0500

    forgot to set the polling period back to something sane

--- src/ota_updateClient.c
+++ src/ota_updateClient.c
@@ -36,8 +36,7 @@
 #define OTA_PORTNUM							443
 #define OTA_VERSION							"v1"
 
-//#define OTA_POLL_PERIOD_MS					5 * 60 * 1000
-#define OTA_POLL_PERIOD_MS					30 * 1000
+#define OTA_POLL_PERIOD_MS					5 * 60 * 1000
 #define INTERBLOCK_DELAY_MS					0
 
 #define UUID_LEN_BYTES						36
