1de4afa0b267bdf32f5d4bd922ba337ba91a9ef8
Christopher Armenio <christopher.armenio@ovrengineered.com>
2018-01-14 21:32:42 -0500

    modified operation so downloads are asynchronous (1 block is downloaded per call to _iterate)

--- src/ota_updateClient.c
+++ src/ota_updateClient.c
@@ -37,7 +37,6 @@
 #define OTA_VERSION							"v1"
 
 #define OTA_POLL_PERIOD_MS					5 * 60 * 1000
-#define INTERBLOCK_DELAY_MS					0
 
 #define UUID_LEN_BYTES						36
 #define SERIALNUM_MAXLEN_BYTES				48
@@ -61,7 +60,9 @@ typedef enum
 
 // ******** local function prototypes ********
 static checkForUpdateRetVal_t isUpdateAvailable(char* updateUuidOut, size_t *const fwSize_bytesOut);
-static void downloadUpdateWithUuid(char *targetUuidIn, size_t fwSize_bytesIn);
+
+static void startUpdateDownloadWithUuid(char *targetUuidIn, size_t fwSize_bytesIn);
+static void download_iterate();
 
 
 // ********  local variable declarations *********
@@ -74,6 +75,14 @@ static ota_updateClient_loggingFunction_t logFunction = NULL;
 static void* logFunction_userVar = NULL;
 
 static uint32_t lastCheckTime_us = 0;
+
+static bool isDownloading = false;
+static char targetFwUuid[UUID_LEN_BYTES+1];
+static size_t currBlockIndex;
+static size_t numBlocks;
+static size_t blockSize_bytes;
+static int numFailedTries = 0;
+
 
 
 // ******** global function implementations ********
@@ -98,6 +107,14 @@ bool ota_updateClient_init(const char *const fwUuidIn,
 
 void ota_updateClient_iterate(void)
 {
+	// if we're currently downloading...keep doing it
+	if( isDownloading )
+	{
+		download_iterate();
+		return;
+	}
+
+	// if we made it here, we're not downloading...check for update periodically
 	uint32_t currTime_us = ota_timeBase_getCount_us();
 	if( ((currTime_us - lastCheckTime_us) / 1000) < OTA_POLL_PERIOD_MS )
 	{
@@ -105,7 +122,7 @@ void ota_updateClient_iterate(void)
 	}
 	lastCheckTime_us = currTime_us;
 
-	// make sure out http client is initialized
+	// make sure our http client is initialized before we check
 	if( !isHttpClientInit )
 	{
 		// initialize our http client
@@ -127,16 +144,11 @@ void ota_updateClient_iterate(void)
 	if( updateRetVal == CHECK_FOR_UPDATE_RETVAL_UPDATE_AVAILABLE )
 	{
 		OTA_LOG_INFO(TAG, "new FW available: '%s'", targetUuid);
+		startUpdateDownloadWithUuid(targetUuid, firmwareSize_bytes);
 	}
 	else if( updateRetVal == CHECK_FOR_UPDATE_RETVAL_UP_TO_DATE )
 	{
 		OTA_LOG_INFO(TAG, "FW up-to-date");
-	}
-
-	// begin our update (if applicable)
-	if( updateRetVal == CHECK_FOR_UPDATE_RETVAL_UPDATE_AVAILABLE )
-	{
-		downloadUpdateWithUuid(targetUuid, firmwareSize_bytes);
 	}
 }
 
@@ -216,91 +228,98 @@ static checkForUpdateRetVal_t isUpdateAvailable(char* updateUuidOut, size_t *con
 }
 
 
-static void downloadUpdateWithUuid(char *targetUuidIn, size_t fwSize_bytesIn)
+static void startUpdateDownloadWithUuid(char *targetUuidIn, size_t fwSize_bytesIn)
 {
 	if( (targetUuidIn == NULL) || (strlen(targetUuidIn) != UUID_LEN_BYTES) ) return;
 
+	isDownloading = true;
+
 	// figure out our block size and get ready for the OTA
-	size_t blockSize_bytes;
 	if( !ota_flash_otaSave_begin(&blockSize_bytes) )
 	{
 		OTA_LOG_ERROR(TAG, "flash reports not ready for OTA, aborting");
+		isDownloading = false;
 		return;
 	}
 
 	// calculate our total number of blocks
-	size_t numBlocks = DIV_ROUND_UP(fwSize_bytesIn, blockSize_bytes);
+	numBlocks = DIV_ROUND_UP(fwSize_bytesIn, blockSize_bytes);
 	OTA_LOG_INFO(TAG, "starting download of %d bytes (%d blocks)", fwSize_bytesIn, numBlocks);
 
+	// save our FW uuid
+	strncpy(targetFwUuid, targetUuidIn, sizeof(targetFwUuid));
+	targetFwUuid[sizeof(targetFwUuid)-1] = 0;
+}
+
+
+static void download_iterate(void)
+{
 	char body[92];
 	char response[blockSize_bytes+1];			// +1 for null-term
 	uint16_t httpStatus;
 
-	// gotta iterate our blocks here
-	int numFailedTries = 0;
-	size_t currBlockIndex = 0;
-	while( currBlockIndex < numBlocks )
+	OTA_LOG_INFO(TAG, "downloading block %d / %d", currBlockIndex+1, numBlocks);
+
+	// create our request body
+	snprintf(body, sizeof(body), "{\"targetFwUuid\":\"%s\",\"offset\":%d,\"maxNumBytes\":%d}", targetFwUuid, currBlockIndex * blockSize_bytes, blockSize_bytes);
+	body[sizeof(body)-1] = 0;
+
+	// issue our request
+	size_t numBytesInCurrBlock;
+	if( ota_httpClient_postJson("/" OTA_VERSION "/devs/getfwdata", body, &httpStatus, response, sizeof(response), &numBytesInCurrBlock, true) )
 	{
-		OTA_LOG_INFO(TAG, "downloading block %d / %d", currBlockIndex+1, numBlocks);
-
-		// create our request body
-		snprintf(body, sizeof(body), "{\"targetFwUuid\":\"%s\",\"offset\":%d,\"maxNumBytes\":%d}", targetUuidIn, currBlockIndex * blockSize_bytes, blockSize_bytes);
-		body[sizeof(body)-1] = 0;
-
-		// issue our request
-		size_t numBytesInCurrBlock;
-		if( ota_httpClient_postJson("/" OTA_VERSION "/devs/getfwdata", body, &httpStatus, response, sizeof(response), &numBytesInCurrBlock, true) )
+		// if we made it here, we got a valid HTTP response...verify it
+		if( httpStatus == 200 )
 		{
-			// if we made it here, we got a valid HTTP response...verify it
-			if( httpStatus == 200 )
+			// got a valid response
+			if( ota_flash_otaSave_writeBlock(response, numBytesInCurrBlock) )
 			{
-				// got a valid response
-				if( ota_flash_otaSave_writeBlock(response, numBytesInCurrBlock) )
-				{
-					currBlockIndex++;
-				}
-				else
-				{
-					OTA_LOG_ERROR(TAG, "flash failed to write block %d, aborting", currBlockIndex);
-					ota_flash_otaSave_abort();
-					ota_httpClient_closeConnection();
-					return;
-				}
+				currBlockIndex++;
 			}
 			else
 			{
-				OTA_LOG_WARN(TAG, "HTTP request did not return 200(OK), will retry");
-				numFailedTries++;
+				OTA_LOG_ERROR(TAG, "flash failed to write block %d, aborting", currBlockIndex);
+				ota_flash_otaSave_abort();
+				ota_httpClient_closeConnection();
+				return;
 			}
 		}
 		else
 		{
-			OTA_LOG_WARN(TAG, "failed to get block %d, will retry", currBlockIndex);
+			OTA_LOG_WARN(TAG, "HTTP request did not return 200(OK), will retry");
 			numFailedTries++;
 		}
-
-		// make sure we don't try _too_ hard
-		if( numFailedTries >= FW_DL_MAX_FAILED_TRIES )
-		{
-			OTA_LOG_ERROR(TAG, "exceeded max # of failures, aborting download");
-			ota_httpClient_closeConnection();
-			return;
-		}
-
-		// give us a bit of a delay between blocks so we don't cause much damage to other processes
-		if( INTERBLOCK_DELAY_MS > 0 ) ota_thread_delay_ms(INTERBLOCK_DELAY_MS);
-	}
-
-	// if we get to here, we're done receiving blocks
-	ota_httpClient_closeConnection();
-	if( ota_flash_otaSave_endAndValidate() )
-	{
-		OTA_LOG_INFO(TAG, "OTA update complete, rebooting");
-		ota_thread_delay_ms(10000);
-		ota_flash_reboot();
 	}
 	else
 	{
-		OTA_LOG_ERROR(TAG, "firmware validation failed");
+		OTA_LOG_WARN(TAG, "failed to get block %d, will retry", currBlockIndex);
+		numFailedTries++;
+	}
+
+	// make sure we don't try _too_ hard
+	if( numFailedTries >= FW_DL_MAX_FAILED_TRIES )
+	{
+		OTA_LOG_ERROR(TAG, "exceeded max # of failures, aborting download");
+		ota_httpClient_closeConnection();
+		isDownloading = false;
+		return;
+	}
+
+	if( currBlockIndex >= numBlocks )
+	{
+		// if we get to here, we're done receiving blocks
+		ota_httpClient_closeConnection();
+		isDownloading = false;
+
+		if( ota_flash_otaSave_endAndValidate() )
+		{
+			OTA_LOG_INFO(TAG, "OTA update complete, rebooting");
+			ota_thread_delay_ms(10000);
+			ota_flash_reboot();
+		}
+		else
+		{
+			OTA_LOG_ERROR(TAG, "firmware validation failed");
+		}
 	}
 }
